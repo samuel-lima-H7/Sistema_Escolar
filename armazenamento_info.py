456616be7198e2fc8bc45abb8bf5718d5ed884a5
Samuel Lima <samuel.aguiar0106@gmail.com>
2026-08-09 14:27:59 -0300

    adição de novas funções de verificação, funcionam para verificar a pré existencia de alunos que seriam cadastrados, assim evitando repetição de nomes

--- armazenamento_info.py
+++ armazenamento_info.py
@@ -1,11 +1,40 @@
 import os
 import csv
+dados = list()
 
-def info_aluno():
-    aluno = input("Qual aluno deseja cadsatrar?: ")
+def info_aluno(aluno):
     with open("data_base.csv", "a", newline="", encoding="utf-8") as armazena:
         armazena_csv = csv.writer(armazena, delimiter=";")
         for c in range(4):
             armazena_csv.writerow([aluno, "", "", "", "", "", "", ""])
 
-info_aluno()
+
+def alunos_ja_cadastrados():
+    alunos =  list()
+    with open("data_base.csv", "r", newline="", encoding="utf-8") as verifica:
+        verifica_csv = csv.reader(verifica, delimiter=";")
+        for cont, valor in enumerate(verifica_csv):
+            if cont == 0 or not valor:
+                continue
+            dados.append(",".join(valor))
+    filtro = dados[0::4]
+
+    for valor_da_linha in filtro:
+        nome = valor_da_linha.split(",") 
+        alunos.append(nome[0])
+
+    return alunos
+
+def verifica_aluno_exstir(alunos_recebido, alunos_existentes):
+    retorno = ""
+    if alunos_recebido in alunos_existentes:
+        retorno =  "aluno já presente em banco de dados"
+    else:
+        retorno = "aluno não identificado no banco de dados, pode ser adicionado"
+
+    return retorno
+    
+
+nome_para_verificar_ou_cadastrar =  input(str("Qual aluno deseja cadastrar?: "))
+print(verifica_aluno_exstir(nome_para_verificar_ou_cadastrar, alunos_ja_cadastrados()))
+
