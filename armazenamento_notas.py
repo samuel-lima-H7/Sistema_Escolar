8ee2339245cdfab86a53d160269ec59f49a934d2
Samuel Lima <samuel.aguiar0106@gmail.com>
2026-08-16 16:13:00 -0300

    inicialização de armazenamento do numero de bimestres

--- armazenamento_notas.py
+++ armazenamento_notas.py
@@ -1,1 +1,19 @@
-import armazenamento_info
+import os
+import csv
+
+def bimestre():
+    while True:
+        try:
+            while True:
+                quantidade_bimestres = int(input("Quais bimestres deseja agregar nota? \nDigite apenas números: ").strip())
+                if quantidade_bimestres > 4:
+                    print("""valor inválido, digite novamente!! 
+                    """)
+                else: 
+                    break
+            break
+        except:
+            print("""valor inválido, digite novamente!!
+            """)
+
+bimestre()
