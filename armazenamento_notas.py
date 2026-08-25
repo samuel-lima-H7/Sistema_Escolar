bf93f81c7edd2554dea3adc397cc20f86df4bba7
Samuel Lima <samuel.aguiar0106@gmail.com>
2026-08-25 10:28:26 -0300

    função que recebe o numero de alunos os quais deseja agragar nota

--- armazenamento_notas.py
+++ armazenamento_notas.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from armazenamento_info import verifica_aluno_exstir, alunos_ja_cadastrados
 
 def bimestre():
     while True:
@@ -15,5 +16,24 @@ def bimestre():
         except:
             print("""valor inválido, digite novamente!!
             """)
+    return quantidade_bimestres
 
-bimestre()
+def materia_escolar():
+    materia = str(input("Qual matéria deseja agregar nota?\nMatérias disponíveis: Matemática, Português, Ciências, Geografia, História, Artes, Educação Física\nDigite a matéria que deseja agregar nota: "))
+
+def alunos_agregar_nota():
+    while True:
+        try:
+            while True:
+                quant = int(input("Digite a quantidade de alunos nos quais deseja agregar nota: "))
+                if quant > len(alunos_ja_cadastrados()):
+                    print("quantidade de alunos inexistentes!")
+                else:
+                    break
+            break
+        except:
+            print("valor inválido, digite novamente!")
+    return quant
+
+alunos_agregar_nota()
+
